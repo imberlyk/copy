003b0362f3e1bac80753157bb985c4bc490ecdde
Kim <77022750+imberlyk@users.noreply.github.com>
2024-03-10 19:31:56 -0400

    huuub

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Function to generate a random position covering the entire viewport
     function getRandomPosition() {
-      const maxX = window.innerWidth - 200; // Adjust according to viewport size
-      const maxY = window.innerHeight - 100; // Adjust according to viewport size
+      const maxX = window.innerWidth - 400; // Adjust according to viewport size
+      const maxY = window.innerHeight - 200; // Adjust according to viewport size
       const x = Math.floor(Math.random() * maxX);
       const y = Math.floor(Math.random() * maxY);
       return { x, y };
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
       let numDivs = Math.ceil(window.innerWidth * window.innerHeight / (200 * 100)); // 
   
       // Triple the number of duplicates
-      numDivs *= 9;
+      numDivs *= 12;
     
       const delayBetweenDuplicates = 100; // milliseconds
     
